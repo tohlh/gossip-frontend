79dfc68ae749838efd67682f0016952ba0a7bde9
tohlh <tohliheng@gmail.com>
2023-01-03 19:20:53 +0800

    user signup and fix login reload

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Layout from './pages/layout';
-import Login from './pages/login';
 import RouteGuard from './components/RouteGuard';
 import { hasAuthToken } from './utils/auth';
+import Login from './pages/login';
+import Signup from './pages/signup';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<RouteGuard pred={!hasAuthToken()} element={<Login />} redirect="/" />} />
+        <Route path="/signup" element={<RouteGuard pred={!hasAuthToken()} element={<Signup />} redirect="/" />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -2,23 +2,26 @@ import '../App.css';
 import React, { useState } from 'react';
 import { Button, Box, Divider, TextField, Typography } from '@mui/material';
 import { Container } from '@mui/system';
-import { login } from '../api/auth';
-import { setAuthToken } from '../utils/auth';
+import { signup } from '../api/auth';
+import { useNavigate } from 'react-router-dom';
 
-const Login: React.FC = () => {
+const Signup: React.FC = () => {
+  const [name, setName] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [loginError, setLoginError] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [signupError, setsignupError] = useState('');
+  const navigate = useNavigate();
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
-    login(username, password)
+    signup(name, username, password, passwordConfirm)
       .then(r => {
-        setAuthToken(r.data.token);
-        window.location.reload();
+        window.alert("Account successfully created!");
+        navigate("/login");
       })
       .catch(e => {
-        setLoginError(e.response.data.error);
+        setsignupError(e.response.data.error);
       })
   }
 
@@ -31,9 +34,16 @@ const Login: React.FC = () => {
         alignItems: 'center'
       }}>
         <Box component="form" onSubmit={handleSubmit}>
-          <h1>Sign in</h1>
-          <p>Welcome back!</p>
-          <Typography color="error"> {loginError} </ Typography>
+          <h1>Sign up</h1>
+          <p>Enter your username and password</p>
+          <Typography color="error"> {signupError} </ Typography>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            onChange={e => setName(e.target.value)}
+            placeholder="Name"
+          />
           <TextField
             margin="normal"
             required
@@ -49,20 +59,28 @@ const Login: React.FC = () => {
             placeholder="Password"
             type="password"
           />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            onChange={e => setPasswordConfirm(e.target.value)}
+            placeholder="Password Confirmation"
+            type="password"
+          />
           <Divider />
-          <p>Don't have an account yet?</p>
-          <a href="/signup">Create one now!</a>
+          <p>Already have an account?</p>
+          <a href="/login">Login here!</a>
           <Button
             fullWidth
             variant="contained"
             type="submit"
             value="Submit"
             sx={{ mt: 3, mb: 2 }}
-          > Sign in </Button>
+          > Sign up </Button>
         </Box>
       </Box>
     </Container>
   );
 }
 
-export default Login;
+export default Signup;
