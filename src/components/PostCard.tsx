f1340bf674058928b01a64edb9c29401de220008
tohlh <tohliheng@gmail.com>
2023-01-04 13:42:42 +0800

    Add PostCard

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,56 @@
+import moment from "moment"
+import { Card, CardContent, CardHeader, Chip, Divider, Typography } from "@mui/material";
+import { post } from "../api/post";
+import { Stack } from "@mui/system";
+
+const PostCard = (
+
+  { id,
+    title,
+    content,
+    is_op,
+    is_edited,
+    tags,
+    user,
+    created_at
+  }: post
+) => {
+  moment.locale("en-SG");
+  const datetime = moment(created_at).format("YYYY/MM/DD, HHmm");
+  const tag_chips = (
+    <Stack spacing={1} direction="row">
+      {tags.map(tag =>
+        <div key={tag.title}>
+          <Chip label={tag.title} size="small" color="primary" />
+        </div>
+      )}
+    </Stack >
+  )
+
+  return (
+    <div>
+      <Card elevation={5} sx={{ borderRadius: 4 }}>
+        <CardHeader align="left" title={title} />
+        <Divider />
+        <CardContent sx={{ textAlign: "left" }}>
+          <Typography variant="body1">{content}</Typography>
+        </CardContent>
+
+        <CardContent sx={{ alignContent: "right" }}>
+          {tag_chips}
+        </CardContent>
+
+        <CardContent sx={{ textAlign: "right" }}>
+          <Typography variant="caption" alignSelf="right">
+            Posted by {user.name} @ {datetime} SGT
+          </Typography>
+          <Typography variant="caption" color="warning">
+            {is_edited ? " (Edited)" : ""}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  )
+};
+
+export default PostCard;
