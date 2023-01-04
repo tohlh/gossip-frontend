a21577f4214c0b0a45744bcd9c59644ced053617
tohlh <tohliheng@gmail.com>
2023-01-04 20:16:18 +0800

    responsive navbar and menu

--- src/components/NavBarDrawer.tsx
+++ src/components/NavBarDrawer.tsx
@@ -1,26 +1,33 @@
 import '../App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {
   AppBar,
   Drawer,
+  IconButton,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
-  Toolbar
+  Toolbar,
+  Typography
 } from '@mui/material';
-import { Tag } from '@mui/icons-material'
+import { MenuOutlined, Tag } from '@mui/icons-material'
 import { useAppSelector, useAppDispatch } from '../store/hooks';
 import { setTagsAsync, selectTags } from '../store/tagSlice';
 
 export default function NavBarDrawer() {
   const tags = useAppSelector(selectTags).tags;
   const dispatch = useAppDispatch();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     dispatch(setTagsAsync());
   }, [dispatch]);
+
+  const handleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
 
   const tags_list = (
     <List sx={{ width: "100%", maxWidth: 280, mt: 10 }}>
@@ -41,10 +48,40 @@ export default function NavBarDrawer() {
     <div>
       <AppBar>
         <Toolbar>
+          <IconButton
+            onClick={handleMenu}
+            sx={{ mr: 2, display: { lg: 'none' } }}
+          >
+            <MenuOutlined />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Gossip
+          </Typography>
         </Toolbar>
       </AppBar>
+      <Drawer variant="permanent"
+        sx={{
+          position: "fixed",
+          display: { xs: "none", lg: "block" },
+          '& .MuiDrawer-paper': { boxSizing: "border-box", width: 230 },
+        }}
+        open
+      >
+        {tags_list}
+      </Drawer>
 
-      <Drawer variant="permanent" sx={{ position: "fixed" }} >
+      <Drawer
+        variant="temporary"
+        onClose={handleMenu}
+        open={menuOpen}
+        ModalProps={{
+          keepMounted: true,
+        }}
+        sx={{
+          display: { xs: "block", lg: "none" },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 230 },
+        }}
+      >
         {tags_list}
       </Drawer>
     </div>
